cfdccdd865e85e089d3b3d38c4e39cb422aaa2c1
Owen Gunden <ogunden@phauna.org>
2013-04-04 17:35:07 -0400

    compatibility fix for older versions of android

--- src/org/phauna/litecoinwidget/BtcPriceCache.java
+++ src/org/phauna/litecoinwidget/BtcPriceCache.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.preference.PreferenceManager;
 import android.content.Context;
+import android.os.Build;
 
 import java.util.Date;
 
@@ -13,20 +14,31 @@ public class BtcPriceCache {
   private Date   mPriceDate;
   private String mPriceExchange;
 
+  private boolean mHasChanged;
+
   public BtcPriceCache(Context c) {
     SharedPreferences globalPrefs = c.getSharedPreferences("global", Context.MODE_PRIVATE);
     mPrice = globalPrefs.getFloat(C.pref_key_rate_price, 0);
     mPriceDate = new Date(globalPrefs.getLong(C.pref_key_rate_date, 0));
     mPriceExchange = globalPrefs.getString(C.pref_key_rate_exchange, "");
+    mHasChanged = false;
   }
 
   public void save(Context c) {
-    SharedPreferences globalPrefs = c.getSharedPreferences("global", Context.MODE_PRIVATE);
-    SharedPreferences.Editor e = globalPrefs.edit();
-    e.putFloat(C.pref_key_rate_price, mPrice);
-    e.putLong(C.pref_key_rate_date, mPriceDate.getTime());
-    e.putString(C.pref_key_rate_exchange, mPriceExchange);
-    e.apply();
+    if (mHasChanged) {
+      SharedPreferences globalPrefs = c.getSharedPreferences("global", Context.MODE_PRIVATE);
+      SharedPreferences.Editor e = globalPrefs.edit();
+      e.putFloat(C.pref_key_rate_price, mPrice);
+      e.putLong(C.pref_key_rate_date, mPriceDate.getTime());
+      e.putString(C.pref_key_rate_exchange, mPriceExchange);
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+        e.apply();
+      }
+      else {
+        e.commit();
+      }
+    }
+    mHasChanged = false;
   }
 
   public float getPrice() {
@@ -62,12 +74,14 @@ public class BtcPriceCache {
       mPrice = newPrice;
       mPriceDate = now;
       mPriceExchange = newPriceExchange;
+      mHasChanged = true;
     } else {
       // otherwise, only update if the exchange is "better"
       if (betterExchange(newPriceExchange, mPriceExchange)) {
         mPrice = newPrice;
         mPriceDate = now;
         mPriceExchange = newPriceExchange;
+        mHasChanged = true;
       }
     }
   }
