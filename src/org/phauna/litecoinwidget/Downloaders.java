1494fc7b1aa9bc312fc4be9ad118d3fd846813bb
Owen Gunden <ogunden@phauna.org>
2013-04-07 01:33:48 -0400

    trust all SSL certificates without verifying

--- src/org/phauna/litecoinwidget/Downloaders.java
+++ src/org/phauna/litecoinwidget/Downloaders.java
@@ -5,6 +5,17 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.MalformedURLException;
+
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import java.util.HashMap;
 import java.util.Date;
 import android.util.Log;
@@ -166,7 +177,15 @@ public class Downloaders {
     HttpURLConnection conn = null;
 
     try {
-      conn = (HttpURLConnection) url.openConnection();
+
+      if (url.getProtocol().toLowerCase().equals("https")) {
+        trustAllHosts();
+        HttpsURLConnection https = (HttpsURLConnection) url.openConnection();
+        https.setHostnameVerifier(DO_NOT_VERIFY);
+        conn = https;
+      } else {
+        conn = (HttpURLConnection) url.openConnection();
+      }
 
       conn.setReadTimeout(10000);
       conn.setConnectTimeout(15000);
@@ -223,5 +242,43 @@ public class Downloaders {
 
     return sb.toString();
   }
+
+  // always verify the host - dont check for certificate
+  final static HostnameVerifier DO_NOT_VERIFY = new HostnameVerifier() {
+    public boolean verify(String hostname, SSLSession session) {
+      return true;
+    }
+  };
+
+  /**
+   * Trust every server - dont check for any certificate
+   */
+  private static void trustAllHosts() {
+    // Create a trust manager that does not validate certificate chains
+    TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+      public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+        return new java.security.cert.X509Certificate[] {};
+      }
+
+      public void checkClientTrusted(X509Certificate[] chain,
+          String authType) throws CertificateException {
+      }
+
+      public void checkServerTrusted(X509Certificate[] chain,
+          String authType) throws CertificateException {
+      }
+    } };
+
+    // Install the all-trusting trust manager
+    try {
+      SSLContext sc = SSLContext.getInstance("TLS");
+      sc.init(null, trustAllCerts, new java.security.SecureRandom());
+      HttpsURLConnection
+          .setDefaultSSLSocketFactory(sc.getSocketFactory());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
 
 }
